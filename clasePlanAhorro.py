e8a464c1f2e72f2ad35702620433836b0979686e
Augusto Manrique <augustomanrique19@gmail.com>
2023-04-24 10:59:14 -0300

    Creacion

---
+++ clasePlanAhorro.py
@@ -0,0 +1,8 @@
+class PlanAhorro:
+    def __init__(self, codigo = 0, modelo = '', version = '', valor = 0, cuotas = 0, licitacion = 0):
+        self.__codigo = codigo
+        self.__modelo = modelo
+        self.__version = version
+        self.__valor = valor
+        self.__cuotas = cuotas
+        self.__licitacion = licitacion
